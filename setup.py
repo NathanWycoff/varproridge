7895ba931c0ce95e3aa3409b5623b9d9a417c528
Nathan Wycoff <nathanbrwycoff@gmail.com>
2019-01-07 14:19:44 -0500

    Repo is now a python module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='Variable-Projection Polynomial Ridge for UQ',
+      url='https://github.com/jeffrey-hokanson/varproridge',
+      author='Jeffrey Hokanson; Nathan Wycoff',
+      author_email='jeffrey@hokanson.us',
+      license='Apache',
+      packages=['varproridge'],
+      zip_safe=False)
